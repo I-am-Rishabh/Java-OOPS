b7500d959c52fae50b92aee1875df9eac08d2004
Rishabh Prasad <111455615+I-am-Rishabh@users.noreply.github.com>
2024-10-07 17:12:44 +0530

    Update concept6.java

--- concept6.java
+++ concept6.java
@@ -1,6 +1,18 @@
 //inheritance
-//1.Single inheritance,2.Hierarchical inheritance,3. Multilevel inheritance
-// 4.Hybrid inheritance
+/**
+Inheritance in Java is a mechanism where one class (called the subclass or child class) 
+    acquires the properties and behaviors (fields and methods) of another class (called the
+superclass or parent class). Inheritance allows for code reuse, method overriding, and 
+    polymorphism,
+    and it helps to establish a natural hierarchical relationship between classes.
+*/
+
+/**
+1.Single inheritance
+2.Hierarchical inheritance
+3. Multilevel inheritance
+4.Hybrid inheritance
+    */
 //import java.util.*;
 //import bank;
 class Shape{
